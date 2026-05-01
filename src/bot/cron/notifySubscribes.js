579b800ecb1f961c73a9cf0edda9f77456d2b876
iaroslav_marchuk <jarik.13a@gmail.com>
2026-05-01 14:49:00 +0100

    add work

--- src/bot/cron/notifySubscribes.js
+++ src/bot/cron/notifySubscribes.js
@@ -2,48 +2,24 @@ import cron from 'node-cron';
 import { SubscriberCollection } from '../../db/models/subscriberModel.js';
 import { getForecast } from '../../services/weatherService.js';
 
-// export const startNotificationCron = (bot) => {
-//   cron.schedule('* * * * *', async () => {
-//     const now = new Date();
-//     const hours = String(now.getUTCHours()).padStart(2, '0');
-//     const minutes = String(now.getUTCMinutes()).padStart(2, '0');
-//     const currentTime = `${hours}:${minutes}`;
-
-//     const subscribers = await SubscriberCollection.find({
-//       notifyAt: currentTime,
-//     });
-
-//     for (const subscriber of subscribers) {
-//       try {
-//         const message = await getForecast(subscriber.city);
-//         await bot.telegram.sendMessage(
-//           subscriber.chatId,
-//           `🌅 Доброго ранку! Ось твій прогноз:\n\n${message}`,
-//         );
-//       } catch (error) {
-//         console.error(
-//           `Помилка відправки для ${subscriber.chatId}:`,
-//           error.message,
-//         );
-//       }
-//     }
-//   });
-
-//   console.log('Cron запущено ✅');
-// };
-
 export const startNotificationCron = (bot) => {
   cron.schedule('* * * * *', async () => {
     const now = new Date();
     const subscribers = await SubscriberCollection.find();
 
     for (const subscriber of subscribers) {
-      // конвертуємо поточний час в часовий пояс користувача
-      const localTime = now.toLocaleTimeString('uk-UA', {
+      const localTime = now.toLocaleTimeString('en-GB', {
         timeZone: subscriber.timezone,
         hour: '2-digit',
         minute: '2-digit',
         hour12: false,
+      });
+
+      console.log({
+        localTime,
+        notifyAt: subscriber.notifyAt,
+        timezone: subscriber.timezone,
+        match: localTime === subscriber.notifyAt,
       });
 
       if (localTime === subscriber.notifyAt) {
@@ -59,4 +35,6 @@ export const startNotificationCron = (bot) => {
       }
     }
   });
+
+  console.log('Cron запущено ✅');
 };

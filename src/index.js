fa065f6a907f3acfbedc2ed51f85492b357fad15
iaroslav_marchuk <jarik.13a@gmail.com>
2026-05-01 14:03:10 +0100

    add work

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import 'dotenv/config';
+import http from 'http';
 
 import { startBot } from './bot/index.js';
 import { initMongoDB } from './db/initMongoDB.js';
+import { getEnvVariable } from './utils/getEnvVariable.js';
+
+const PORT = getEnvVariable('PORT') || 8080;
 
 const bootstrap = async () => {
   await initMongoDB();
   await startBot();
+
+  http.createServer((_, res) => res.end('ok')).listen(PORT);
 };
 
 bootstrap().catch((error) => console.error(error));

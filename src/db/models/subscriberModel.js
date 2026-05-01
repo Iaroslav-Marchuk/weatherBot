189031c82c8c3e5ca270edab5c84794d9ca1e13f
iaroslav_marchuk <jarik.13a@gmail.com>
2026-05-01 15:04:50 +0100

    add work

--- src/db/models/subscriberModel.js
+++ src/db/models/subscriberModel.js
@@ -5,6 +5,7 @@ const subscriberModel = new Schema(
     chatId: { type: Number, required: true, unique: true },
     city: { type: String, required: true },
     notifyAt: { type: String, required: true },
+    timezone: { type: String, required: true },
   },
   { timestamps: false, versionKey: false },
 );
